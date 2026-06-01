52802b297a5c0d402ea2cf50df7e5932d225573c
Cristian Orellana <cristianj.orellanaa@gmail.com>
2026-05-31 22:32:41 -0400

    Arreglo de carpetas

--- Producto/backend macrofit/src/main/java/com/proyecto/macrofit/usuarios/repository/NvActividadRepository.java
+++
@@ -1,8 +0,0 @@
-package com.proyecto.macrofit.usuarios.repository; // Ajusta tu paquete
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.proyecto.macrofit.usuarios.model.NvActividad;
-
-public interface NvActividadRepository extends JpaRepository<NvActividad, Integer> {
-}
--- Producto/backend macrofit/src/main/java/com/proyecto/macrofit/usuarios/repository/TipoAlimentacionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.proyecto.macrofit.usuarios.repository;
-
-import com.proyecto.macrofit.usuarios.model.TipoAlimentacion;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TipoAlimentacionRepository extends JpaRepository<TipoAlimentacion, Integer> {
-}
